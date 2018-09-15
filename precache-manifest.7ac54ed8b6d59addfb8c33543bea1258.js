30254f26f20f845530b58cb639999b64fafef9fd
hontas <lundin.pontus@gmail.com>
2018-09-15 09:34:57 +0200

    deploy

---
+++ precache-manifest.7ac54ed8b6d59addfb8c33543bea1258.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1a21cce637b70098fc3c",
+    "url": "/rasmus2.0/js/chunk-vendors-legacy.c3bfdf4d.js"
+  },
+  {
+    "revision": "d9b4935364554361cb0f",
+    "url": "/rasmus2.0/js/chunk-4c1e-legacy.01206284.js"
+  },
+  {
+    "revision": "29a33d60d7bc744ea1f5",
+    "url": "/rasmus2.0/js/app-legacy.4a04eb91.js"
+  },
+  {
+    "revision": "a827f37cbb3a9d6e147cfcb5d12c1e1f",
+    "url": "/rasmus2.0/index.html"
+  },
+  {
+    "revision": "d9b4935364554361cb0f",
+    "url": "/rasmus2.0/css/chunk-4c1e.d1075188.css"
+  },
+  {
+    "revision": "29a33d60d7bc744ea1f5",
+    "url": "/rasmus2.0/css/app.b0e3ef30.css"
+  }
+];
---
+++ precache-manifest.9dab4793051d8d2a8e58a34b77ae1773.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/rasmus2.0/robots.txt"
+  },
+  {
+    "revision": "73f8bf6fa7279ec37523",
+    "url": "/rasmus2.0/js/chunk-vendors.8296e375.js"
+  },
+  {
+    "revision": "dc58aed6b1459ca56c0a",
+    "url": "/rasmus2.0/js/chunk-4c1e.a915e4aa.js"
+  },
+  {
+    "revision": "12b01cfbb50766b97265",
+    "url": "/rasmus2.0/js/app.96247003.js"
+  },
+  {
+    "revision": "717a5680c518a86c43fd4dc1ff565e8e",
+    "url": "/rasmus2.0/index.html"
+  },
+  {
+    "revision": "dc58aed6b1459ca56c0a",
+    "url": "/rasmus2.0/css/chunk-4c1e.d1075188.css"
+  },
+  {
+    "revision": "12b01cfbb50766b97265",
+    "url": "/rasmus2.0/css/app.b0e3ef30.css"
+  }
+];
