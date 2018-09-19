cfb5b3f443951e505ceee3b9ea5fc8f71587520d
hontas <lundin.pontus@gmail.com>
2018-09-19 21:50:06 +0200

    add bugsnag

--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
 import 'fg-loadcss';
+import bugsnag from 'bugsnag-js';
 import Vue from 'vue';
+import bugsnagVue from 'bugsnag-vue';
+
+const bugsnagClient = bugsnag('3d7128669b3309e9907cceeae33c88d6');
+bugsnagClient.use(bugsnagVue(Vue));
 
 import App from './App.vue';
 import store from './store';
