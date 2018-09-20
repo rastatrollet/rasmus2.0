c18c94f996692edeb81e858d0916b396a0dca8e5
hontas <lundin.pontus@gmail.com>
2018-09-21 00:52:44 +0200

    add generic button component

---
+++ src/components/Button.vue
@@ -0,0 +1,59 @@
+<template>
+  <button
+    :class="[$style.button, className]"
+    :type="type"
+    :title="title"
+    @click.prevent="onClick">
+    {{ text }}
+    <font-awesome 
+      v-if="icon" 
+      :icon="['fas', icon]" />
+  </button>
+</template>
+<script>
+export default {
+  name: 'Button',
+  props: {
+    className: {
+      type: String,
+      default: ''
+    },
+    type: {
+      type: String,
+      default: 'button'
+    },
+    title: {
+      type: String,
+      default: ''
+    },
+    onClick: {
+      type: Function,
+      default: () => {}
+    },
+    text: {
+      type: String,
+      default: ''
+    },
+    icon: {
+      type: String,
+      default: null
+    }
+  }
+};
+</script>
+<style module>
+.button {
+  background: transparent;
+  border: none;
+  color: inherit;
+  font: inherit;
+  margin: 0;
+  overflow: visible;
+  padding: 0.5em 1em;
+  user-select: none;
+  width: auto;
+}
+.button:focus {
+  outline: none;
+}
+</style>
